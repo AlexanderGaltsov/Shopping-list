dbe8115f6b6702d8fee61d697d18a71a6132a9e4
AlexanderGaltsov <gau051177@gmail.com>
2022-09-19 04:07:37 +0300

    Внесены исправления после проверки

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,20 @@ import java.util.ArrayList;
 
 public class Main {
 
+    static List<String> basket = new ArrayList<>();
+
+    private static void selectList() {
+        for (int i = 0; i < basket.size(); i++) {
+            System.out.println((i + 1) + ". " + basket.get(i));
+        }
+    }
+    public static boolean isNumeric(String choice) {
+        return choice.matches("-?\\d+(\\.\\d+)?");
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        List<String> basket = new ArrayList<>();
         List<String> find = new ArrayList<>();
         List<Integer> room = new ArrayList<>();
 
@@ -21,57 +31,52 @@ public class Main {
             System.out.println("3. Удалить ");
             System.out.println("4. Найти ");
             System.out.println("Для выхода из программы введите end");
+            System.out.println();
             String input = scanner.nextLine();
             if ("end".equals(input)) {
                 break;
             }
 
-            int x = Integer.parseInt(input);
+            int itemSelect = Integer.parseInt(input);
 
-            switch (x) {
+            switch (itemSelect) {
                 case 1:
                     System.out.println("Какую покупку хотите добавить ? ");
+                    System.out.println();
                     String choice = scanner.nextLine();
                     basket.add(choice);
                     System.out.println("Итого в списке покупок " + basket.size());
+                    System.out.println();
                     break;
                 case 2:
                     System.out.println("Список покупок : ");
-                    for (int i = 0; i < basket.size(); i++) {
-                        System.out.println((i + 1) + ". " + basket.get(i));
-                    }
+                    selectList();
                     break;
                 case 3:
                     int productNumber;
                     System.out.println("Список покупок : ");
-                    for (int i = 0; i < basket.size(); i++) {
-                        System.out.println((i + 1) + ". " + basket.get(i));
-                    }
+                    System.out.println();
+                    selectList();
                     System.out.println("Какую покупку хотите удалить ? Введите номер или название ");
                     System.out.println();
                     choice = scanner.nextLine();
 
-                    if (new Scanner(choice).hasNextInt()) {
+                    if (isNumeric(choice)) {
                         productNumber = Integer.parseInt(choice);
                         System.out.println("Покупка \"" + basket.get(productNumber - 1) + "\" удалена, список покупок: ");
                         basket.remove(productNumber - 1);
-                        for (int i = 0; i < basket.size(); i++) {
-                            System.out.println((i + 1) + ". " + basket.get(i));
-                        }
+                        selectList();
                     } else {
                         System.out.println();
                         System.out.println("Покупка \"" + choice + "\" удалена, список покупок: ");
                         basket.remove(choice);
-                        for (int i = 0; i < basket.size(); i++) {
-                            System.out.println((i + 1) + ". " + basket.get(i));
-                        }
+                        selectList();
                     }
                     break;
                 case 4:
                     System.out.println("Введите текст для поиска: ");
                     choice = scanner.nextLine();
-                    int i = 0;
-                    for (i = 0; i < basket.size(); i++) {
+                    for (int i = 0; i < basket.size(); i++) {
                         String choiceLower = choice.toLowerCase();
                         String basketLower = basket.get(i).toLowerCase();
                         if (basketLower.contains(choiceLower)) {
@@ -89,4 +94,5 @@ public class Main {
             }
         }
     }
+
 }
